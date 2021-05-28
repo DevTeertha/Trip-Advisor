22c4235f55f23966e578d778767d3100cf7c677b
Dev Teertha <devteertha28@gmail.com>
2021-05-28 18:00:04 +0600

    first commit

---
+++ src/redux/reducers/Reducers.js
@@ -0,0 +1,10 @@
+const initialState = {
+    cart: []
+}
+
+export const Reducers = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
