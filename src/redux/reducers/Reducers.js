36c7bab385411b2bc57d2f3fab492781edcb96f9
Dev Teertha <devteertha28@gmail.com>
2021-05-29 00:14:04 +0600

    some change

--- src/redux/reducers/Reducers.js
+++ src/redux/reducers/Reducers.js
@@ -1,9 +1,18 @@
 const initialState = {
-    cart: []
+    hotels: [],
+    result: [],
+    bookList:[]
 }
 
 export const Reducers = (state = initialState, action) => {
     switch(action.type){
+        case 'BOOK_NOW':{
+            const newState = {
+                ...state,
+                bookList: [...state.bookList, action.id]
+            }
+            return newState;
+        }
         default:
             return state;
     }
