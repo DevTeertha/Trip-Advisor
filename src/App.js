42127f8fb805305ce366364a692ea297e2e45d8e
Dev Teertha <devteertha28@gmail.com>
2021-05-29 23:50:18 +0600

    some change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import './App.css';
-<<<<<<< HEAD
 import Login from './Components/Login/Login/Login';
 import { createContext, useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-export const UserContext = createContext()
-
-=======
-import Home from './Components/Home/Home';
 import {
   BrowserRouter as Router,
   Switch,
@@ -20,47 +8,33 @@ import {
 } from "react-router-dom";
 import Search from './Components/Search/Search';
 import BookNow from './Components/BookNow/BookNow';
->>>>>>> Trial
+
+export const UserContext = createContext()
+
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState({})
   return (
-<<<<<<< HEAD
     <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
-<Router>
-      <Switch>
-        <Route exact path="/">
-
-        </Route>
-        <Route path="/login">
-        <Login></Login>
-        </Route>
-        <Route path="/dashboard">
-   
-        </Route>
-      </Switch>
-      </Router>
-
-     
-    </UserContext.Provider>
-=======
       <Router>
         <Switch>
           <Route exact path="/">
-            <Home></Home>
           </Route>
-          <Route path="/home">
-            <Home></Home>
+          <Route path="/login">
+            <Login></Login>
           </Route>
           <Route path="/search">
             <Search></Search>
+          </Route>
+          <Route path="/dashboard">
+
           </Route>
           <Route path="/:id">
             <BookNow></BookNow>
           </Route>
         </Switch>
       </Router>
->>>>>>> Trial
+    </UserContext.Provider>
   );
 }
 
