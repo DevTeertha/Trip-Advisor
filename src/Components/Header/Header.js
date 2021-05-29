0edeb6beafc1b17766b1af466ac01adc5397746f
Dev Teertha <devteertha28@gmail.com>
2021-05-29 19:28:22 +0600

    navbar design change

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,31 +1,11 @@
 import React from 'react';
-import { Container, Nav } from 'react-bootstrap';
-import { Link } from "react-router-dom"
-import './Header.css'
+import Navbar from '../Navbar/Navbar';
 
 const Header = () => {
     return (
-        <Container className="header pt-4">
-            <div>
-                <Link to="/">Trip-Advisor</Link>
-            </div>
-            <div>
-                <Nav className="justify-content-end" activeKey="/home">
-                    <Nav.Item>
-                        <Nav.Link ><Link to="/home">Home</Link></Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link ><Link to="/experience">Experience</Link></Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link ><Link to="/help">Help</Link></Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <Nav.Link><Link to="/login">LogIn</Link></Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </div>
-        </Container>
+        <div>
+            <Navbar></Navbar>
+        </div>
     );
 };
 
